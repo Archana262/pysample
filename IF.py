804adeddcda0d9fd68f09341410c3d11d90920c9
Archana <archanamanoj208@gmail.com>
2025-06-17 10:32:15 +0530

    If statement

--- IF.py
+++ IF.py
@@ -1,4 +1,66 @@
-age = 18
+# age = 18
 
-if age >= 18:
-    print("You are an adult.")
+# if age >= 18:
+#     print("You are an adult.")
+
+
+
+# age = 20
+# has_id = True
+
+# if age >= 18:
+#     if has_id:
+#         print("You are allowed to enter.")
+#     else:
+#         print("You need to show your ID.")
+# else:
+#     print("You are not allowed to enter.")
+
+
+   #if else statement
+# i = 20
+# if i < 15:
+# 	print("i is smaller than 15")
+#     print("i'm in if Block")
+# else:
+# 	print("i is greater than 15")
+# 	print("i'm in else Block")
+# print("i'm not in if and not in 	else Block")
+
+
+#nested if
+# i = 10
+# if i == 10:
+#     if i < 15:
+#         print("i is smaller than 15")
+#     if i < 12:
+#         print("i is smaller than 12 too")
+#     else:
+#          print("i is greater than 15")
+
+
+# i=20
+# if i==10:
+#    print("i is 10")
+# elif(i==15):
+#    print("i is 15")
+# elif(i==20):
+#    print("i is 20")
+# else:
+#     print("i is not present")
+
+
+i=90
+if i<=100 and i>=0:
+    if i>=90:
+        print("A grade")
+    elif i>=80:
+        ptint("B grade")
+    elif i>=70:
+        ptint("C grade")
+    elif i>=60:
+        print("D grade")
+    else:
+        print("Fail")
+else:
+        print("invalid mark")
