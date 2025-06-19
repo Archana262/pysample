e8ab0959f60e3a6b5d315dadcbfcb13a58b43b3e
Archana <archanamanoj208@gmail.com>
2025-06-19 23:00:42 +0530

    loop statements

---
+++ loop.py
@@ -0,0 +1,77 @@
+# count=1
+# while count<=5:
+#     print(count)
+#     count+=1
+
+
+# count=1
+# while count<=10:
+#     if count%2==0:
+#         print(count)
+#     count+=1
+
+
+# count=10
+# while count>=1: 
+#     print(count)
+#     count-=1
+
+
+# a=0
+# i=int(input("enter the number"))
+# while (i!=-1):
+#     a=a+i
+#     i=int(input("enter the number"))
+# print(a)
+
+
+#sum of the digits using a while loop
+# sum=0
+# n=0
+# i=int(input("enter the number"))
+# while i>0:
+#     n=i%10
+#     sum+=n
+#     i=i//10
+# print(sum)
+
+
+#check the number is palindrome
+# temp=0
+# rev=0
+# i=int(input("enter a number"))
+# temp=i
+# while temp>0:
+#     n=temp%10
+#     rev=rev*10+n
+#     temp=temp//10
+# if rev==i:
+#     print("It is a palindrome number")
+# else:
+#     print("It is not a palindrome number")
+
+
+#count the number of digits#Sum of digits
+# n=0
+# count=0
+# i=int(input("enter a number"))
+# while i>0:
+#     n=i%10
+#     count +=1
+#     i=i//10
+# print(count)
+
+
+#check for armstrong number
+# n=0
+# sum=0
+# i=int(input("enter a number"))
+# temp=i
+# while temp>0:
+#     n=temp%10
+#     sum+=n**3
+#     temp=temp//10
+# if sum==i:
+#     print("it is an armstrong number")
+# else:
+#         print("it is not an armstrog number")
