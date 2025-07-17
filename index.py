d477ec03cf1994f18143259dc07e8f602d38c5ad
Archana <archanamanoj208@gmail.com>
2025-07-17 10:23:37 +0530

    index

---
+++ index.py
@@ -0,0 +1,189 @@
+# a="hello World"
+# print(a[::-1])
+
+# a[1]="y"
+# print(a)
+
+
+# message='python is fun'
+# print(message.lower())
+
+
+# message='python is fun'
+# print(message.upper())
+
+# txt="malayalam is my favorite language"
+# x=txt.count("a")
+# print(x)
+
+# message='python is a fun lagguage'
+# print(message.find("fun"))
+
+# text='foot ball'
+# replaced_text=text.replace('foot','volley')
+# print(replaced_text)
+
+# numbers=[37,47,67]
+# print("Before Append:",numbers)
+# numbers.append(57)
+# print("After Append:",numbers)
+
+# vowel=['a','e','i','u']
+# vowel.insert(3,'o')
+# print('list:',vowel)
+
+
+# prime_numbers=[2,3,5]
+# print("list1:",prime_numbers)
+# even_numbers=[4,6,8]
+# print("list2:",even_numbers)
+# prime_numbers.extend(even_numbers)
+# print("list after append:",prime_numbers)
+
+
+# languages=['python','swift','c++']
+# languages[2]='c'
+# print(languages)
+
+
+# a=[]
+# b=int(input("how many values to enter: "))
+# for i in range(0,b):
+#     value=int(input("enter your values: "))
+#     a.append(value)
+# print(a)
+
+
+
+# a=[]
+# n=int(input("enter the number: "))
+# for i in range(0,n):
+#     if i%2==0:
+#         a.append(i)
+# # print(a)
+#  if i %2==0:
+
+
+
+# prime_numbers=[2,3,5,7]
+# removed_elements=prime_numbers.pop(2)
+# print('removed elemets',removed_elements)
+# print('updated list,'prime numbers)
+
+
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# for i in range(len(a)):
+#     if i<=len(a)-1:
+#         if a[i] % 2 == 0:
+#             a.pop(i)
+# print("list=",a)
+
+# languages=['python','swift','c++','c','java','rust','R']
+# del languages[1]
+# print (languages)
+
+# languages=['python','c++','c','java','rust','R']
+# del languages[-1]
+# print (languages)
+# languages=['python','c++','c','java','rust','R']
+# del languages[0:2]
+# print(languages)
+
+# languages=['python','swift','c++','c','java','rust','R']
+# languages.remove('python')
+# print(languages)
+
+# prime_numbers=[2,3,5,7]
+# prime_numbers.reverse()
+# print('reversed list:',prime_numbers)
+
+# list1=[12,14,16,18,20]
+# l=list1*2
+# print(l)
+
+# list1=[12,14,16,18,20]
+# list2=[9,10,32,54,86]
+# l=list1 + list2
+# print(l)
+
+# list1=[12,2,3,4,5,65,12,33,29]
+# a=len(list1)
+# print(a)
+
+
+# list1=[12,14,16,18,20]
+# for i in list1:
+#     print(i)
+
+
+# list1=["john","david","james"]
+# for i in list1:
+#     print(i)
+
+
+
+# list1=[100,200,300,400,500]
+# print(600 in list1)
+# print(700 in list1)
+
+
+# list1=[103,434,5645,678,432]
+# print(max(list1))
+
+
+# list1=[103,543,678,546,3]
+# print(min(list1))
+
+
+# list1=[1,2,3,4,5]
+# list2=[3,4,5,6,7]
+# intersection1=set(list1).intersection(list2)
+# print(intersection1)
+
+
+# list1=[1,2,3,4,5,]
+# list2=[3,4,5,6,7]
+# intersection2=set(list1) & set(list2)
+# print(intersection2)
+
+# list1=[1,2,3,4,5]
+# list2=[2,3,4,5,6]
+# a=[]
+# for i in range(len(list1)):
+#     for j in range(len(list2)):
+#         if list1[i]==list2[j]:
+#             a.append(list2[i])
+# print(a)
+
+
+
+# list1=[232,343,4545,6767,234]
+# greatest=list1[0]
+# for i in range(len(list1)):
+#     if list1[i]>greatest:
+#         greatest=list1[i]
+# print(greatest)
+
+
+
+# list1=[232,343,4545,6767,234]
+# smallest=list1[0]
+# for i in range(len(list1)):
+#     if list1[i]<smallest:
+#         smallestest=list1[i]
+# print(smallest)
+
+
+
+# numbers=[1,2,3,4,5]
+# a=[x**2 for x in numbers]
+# print(a)
+
+    
+# matrix=[[j for j in range(3)]for i in range(3)]    
+# print(matrix)
+
+
+
+
